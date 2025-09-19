988df81bb29f5089133fbe70d829557d37de705c
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2025-09-19 11:57:59 -0300

    comecei estilizacao

--- Projeto_Contatos/src/App.jsx
+++ Projeto_Contatos/src/App.jsx
@@ -3,6 +3,8 @@ import Create from './componentes/Create'
 import ListContatos from './componentes/ListContatos'
 import Search from './componentes/Search'
 import './App.css'
+import './theme.css'
+import './global.css'
 
 function App() {
   const [Contatos, setContatos] = useState([])
@@ -63,7 +65,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className='container'>
 
       <div>
         <Create 
