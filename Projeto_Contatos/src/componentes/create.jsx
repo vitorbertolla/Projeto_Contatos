92f75d36b6cd9fcb48febb0eb77a4c1d6d5ca140
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2025-09-19 17:54:10 -0300

    prettier

--- Projeto_Contatos/src/componentes/create.jsx
+++ Projeto_Contatos/src/componentes/create.jsx
@@ -1,63 +1,64 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const Create = ({ addContato, ContatosEdit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-    const [name, setName] = useState("")
-    const [number, setNumber] = useState("")
+  const handleNumberChange = (event) => {
+    let value = event.target.value;
 
-    const handleNumberChange = (event) => {
-        let value = event.target.value;
+    value = value.replace(/\D/g, "");
 
-        value = value.replace(/\D/g, "");
-
-        // Limita a 11 caracteres
-        if (value.length <= 11) {
-            setNumber(value);
-        }
+    // Limita a 11 caracteres
+    if (value.length <= 11) {
+      setNumber(value);
     }
+  };
 
-    useEffect(() => {
-        if (ContatosEdit) {
-            setName(ContatosEdit.name)
-            setNumber(ContatosEdit.number.replace(/\D/g, "")) 
-        } else {
-            setName("")
-            setNumber("")
-        }
-    }, [ContatosEdit])
-
-    const Submit = async (e) => {
-        e.preventDefault()
-        if (!name || !number) return
-        else {
-            const success = addContato(name, number)
-            if (success) {
-                setName("")
-                setNumber("")
-            }
-        }
+  useEffect(() => {
+    if (ContatosEdit) {
+      setName(ContatosEdit.name);
+      setNumber(ContatosEdit.number.replace(/\D/g, ""));
+    } else {
+      setName("");
+      setNumber("");
     }
+  }, [ContatosEdit]);
 
-    return (
-        <div className="container-add">
-            <h1>Adicionar Contato</h1>
-            <form onSubmit={Submit}>
-                <input 
-                    type="text" 
-                    placeholder="Nome:" 
-                    value={name} 
-                    onChange={(e) => setName(e.target.value)} 
-                />
-                <input 
-                    type="text" 
-                    placeholder="Número:" 
-                    value={number} 
-                    onChange={handleNumberChange} 
-                />
-                <button type="submit">{ContatosEdit ? "Salvar Alterações" : "Adicionar Contato" }</button>
-            </form>
-        </div>
-    )
-}
+  const Submit = async (e) => {
+    e.preventDefault();
+    if (!name || !number) return;
+    else {
+      const success = addContato(name, number);
+      if (success) {
+        setName("");
+        setNumber("");
+      }
+    }
+  };
 
-export default Create
+  return (
+    <div className="container-add">
+      <h1>Adicionar Contato</h1>
+      <form onSubmit={Submit}>
+        <input
+          type="text"
+          placeholder="Nome:"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Número:"
+          value={number}
+          onChange={handleNumberChange}
+        />
+        <button type="submit">
+          {ContatosEdit ? "Salvar Alterações" : "Adicionar Contato"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
