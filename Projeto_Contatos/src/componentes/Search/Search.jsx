5ddd05ee3f2adf1b3dad4d9fae1649e2ec7de27f
Vitor Hugo Almeida Bertolla <163414035+vitorbertolla@users.noreply.github.com>
2025-10-16 07:21:28 -0300

    Pequena modificação no h1

--- Projeto_Contatos/src/componentes/Search/Search.jsx
+++ Projeto_Contatos/src/componentes/Search/Search.jsx
@@ -1,14 +1,13 @@
 const Search = ({ContatosSearch, setSearch,Contatos }) => {
     return(
     <div className="form-control search-control">
-      <h1>Seus Contatos</h1>
+      <h1>Seus Contatos ({Contatos.length})</h1>
       <input
         type="text"
         value={ContatosSearch}
         placeholder="Nome ou Número"
         onChange={(e) => setSearch(e.target.value)}
       />
-      <h3>Seus Contatos ({Contatos.length})</h3>
     </div>
     )
 }
